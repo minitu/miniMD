924320f6b7a2ab872d80ed5150b41281fc80530a
Jaemin Choi <jchoi157@illinois.edu>
2020-08-25 11:47:20 -0400

    Add nsys script on Summit

--- charm/scripts/summit/nsys.sh
+++ charm/scripts/summit/nsys.sh
@@ -2,7 +2,7 @@
 #BSUB -P csc357
 #BSUB -W 0:10
 #BSUB -nnodes 1
-#BSUB -J minimd-nsys-n4
+#BSUB -J minimd-nsys-n4-c8
 
 date
 
@@ -15,5 +15,6 @@ ny=128
 nz=128
 n_iters=10
 pemap="L0,4,84,88"
+options="-gn 0"
 
-jsrun -n$n_procs -a1 -c1 -g1 -K2 -r4 nsys profile -f true -o miniMD-n$n_procs-c$n_chares-nx$nx-ny$ny-nz$nz-p%q{OMPI_COMM_WORLD_RANK} ./miniMD -c $n_chares -i ../inputs/in.lj.miniMD -nx $nx -ny $ny -nz $nz -n $n_iters +ppn 1 +pemap $pemap
+jsrun -n$n_procs -a1 -c1 -g1 -K2 -r4 nsys profile -f true -o miniMD-n$n_procs-c$n_chares-nx$nx-ny$ny-nz$nz-p%q{OMPI_COMM_WORLD_RANK} ./miniMD -c $n_chares -i ../inputs/in.lj.miniMD -nx $nx -ny $ny -nz $nz -n $n_iters $options +ppn 1 +pemap $pemap
