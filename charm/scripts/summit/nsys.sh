0ae631f9f86793e82ea9836458527de1465129a9
Jaemin Choi <jchoi157@illinois.edu>
2020-08-25 00:22:57 -0400

    Add clean-nsys to Makefile

---
+++ charm/scripts/summit/nsys.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#BSUB -P csc357
+#BSUB -W 0:10
+#BSUB -nnodes 1
+#BSUB -J minimd-nsys-n4
+
+date
+
+cd $MEMBERWORK/csc357/miniMD/charm
+
+n_procs=4
+n_chares=8
+nx=128
+ny=128
+nz=128
+n_iters=10
+pemap="L0,4,84,88"
+
+jsrun -n$n_procs -a1 -c1 -g1 -K2 -r4 nsys profile -f true -o miniMD-n$n_procs-c$n_chares-nx$nx-ny$ny-nz$nz-p%q{OMPI_COMM_WORLD_RANK} ./miniMD -c $n_chares -i ../inputs/in.lj.miniMD -nx $nx -ny $ny -nz $nz -n $n_iters +ppn 1 +pemap $pemap
