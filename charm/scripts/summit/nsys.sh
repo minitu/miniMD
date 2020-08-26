e136c5df110a89c33e8c7955501298e32d425853
Jaemin Choi <jchoi157@illinois.edu>
2020-08-26 00:01:30 -0400

    Update Summit scripts

--- charm/scripts/summit/nsys.sh
+++ charm/scripts/summit/nsys.sh
@@ -9,12 +9,12 @@ date
 cd $MEMBERWORK/csc357/miniMD/charm
 
 n_procs=4
-n_chares=8
+n_chares=16
 nx=128
 ny=128
 nz=128
-n_iters=10
+n_iters=100
 pemap="L0,4,84,88"
 options="-gn 0"
 
-jsrun -n$n_procs -a1 -c1 -g1 -K2 -r4 nsys profile -f true -o miniMD-n$n_procs-c$n_chares-nx$nx-ny$ny-nz$nz-p%q{OMPI_COMM_WORLD_RANK} ./miniMD -c $n_chares -i ../inputs/in.lj.miniMD -nx $nx -ny $ny -nz $nz -n $n_iters $options +ppn 1 +pemap $pemap
+jsrun -n$n_procs -a1 -c1 -g1 -K2 -r4 nsys profile -t cuda,nvtx -s none -b none -f true -o miniMD-n$n_procs-c$n_chares-nx$nx-ny$ny-nz$nz-p%q{OMPI_COMM_WORLD_RANK} ./miniMD -c $n_chares -i ../inputs/in.lj.miniMD -nx $nx -ny $ny -nz $nz -n $n_iters $options +ppn 1 +pemap $pemap
