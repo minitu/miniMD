04c6708db5958a8140338e6f82b9025d523726ff
Jaemin Choi <jchoi157@illinois.edu>
2020-08-28 01:08:19 -0400

    Add date to Summit scripts

--- charm/scripts/summit/benchmark.sh
+++ charm/scripts/summit/benchmark.sh
@@ -31,6 +31,7 @@ do
   echo -e "# ODF-$overdecomp\n"
   for iter in 1 2 3
   do
+    date
     echo -e "# Run $iter\n"
     exe jsrun -n$n_procs -a1 -c$ppn -g1 -K3 -r6 ./$file -c $num_chares $options -nx $nx -ny $ny -nz $nz -n $n_iters +ppn $ppn +pemap $pemap
   done
